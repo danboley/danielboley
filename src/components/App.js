0c2e959a969b809c4aac2e64e14ea6647535ae28
Daniel Boley <danboley1@gmail.com>
2023-03-29 16:44:57 -0400

    created base components, linked to navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from './Home';
 import TechProjects from './TechProjects';
+import About from "./About";
+import Skills from "./Skills";
+import Contact from "./Contact";
 
 function App() {
   return (
@@ -9,8 +12,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/techprojects" element={<TechProjects />} />
-          <Route path="/testing" />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,10 @@ function Navbar() {
   return (
     <div>Navbar
         <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
         <Link to="/techprojects">Tech Projects</Link>
+        <Link to="/skills">Skills</Link>
+        <Link to="/contact">Contact</Link>
     </div>
   )
 }
