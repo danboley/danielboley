cd33e0227f8bf810a278855f60f317d09992c879
Daniel Boley <danboley1@gmail.com>
2023-03-30 16:48:31 -0400

    added react icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { SiLinkedin, SiGithub } from "react-icons/si";
+import { MdOutlineEmail } from "react-icons/md";
 
 function Contact() {
   return (
     <div>
       <h1>Contact Info</h1>
       <h3>Feel free to get connect with me or get in touch</h3>
+      <SiLinkedin />
       <h4>LinkedIn</h4>
+      <MdOutlineEmail />
       <h4>Email</h4>
+      <SiGithub />
       <h4>GitHub</h4>
     </div>
   )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { SiJavascript, SiReact, SiRuby, SiRubyonrails, SiRubysinatra, SiHtml5, SiCss3, SiTailwindcss, SiGithub, SiGithubpages, SiPostgresql, SiPostman } from "react-icons/si";
 
 function Skills() {
-  return (
-    <div>Skills</div>
-  )
+  return <div>Skills
+    <SiJavascript />
+    <SiReact />
+    <SiRuby />
+    <SiRubyonrails />
+    <SiRubysinatra />
+    <SiHtml5 />
+    <SiCss3 />
+    <SiTailwindcss />
+    <SiPostgresql />
+    <SiPostman />
+    <SiGithub />
+    <SiGithubpages />
+
+
+  </div>;
 }
 
-export default Skills
+export default Skills;
