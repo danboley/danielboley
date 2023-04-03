d21b16eed3e101b430b030a0ca27f069e374cfaf
Daniel Boley <danboley1@gmail.com>
2023-04-03 11:28:30 -0400

    tailwind test

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <h1>Howdy! I'm Dan! </h1>
+        <h1 className="text-3xl font-bold underline"> Howdy! I'm Dan! </h1>
         <h2>I'm a full stack software engineer and I built this website to display some of the projects I've created, show off my technical skills, and share some of my other interests.</h2>
         <img src=""></img>
     </div>
