64cbceafd6a801de8578ca1f1250f8f057f9c3b8
Daniel Boley <danboley1@gmail.com>
2023-04-04 10:53:26 -0400

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,27 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>
-        <Link to="/">Daniel Boley</Link>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/techprojects">Technical Projects</Link>
-        <Link to="/skills">Skills</Link>
-        <Link to="/contact">Contact</Link>
+    <div className="bg-black text-white fixed w-full h-20 flex justify-between items-center p-4 sm:p-6 font-bold">
+        <Link to="/">
+          <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl transition ease-in-out hover:scale-110">Daniel Boley</h1>
+        </Link>
+        <div className="space-x-2 flex items-center md:space-x-4">
+          <Link className="focus:text-gray-300" to="/">
+            <h2 className="text-xl md:text-3xl transition ease-in-out hover:scale-110">Home</h2>
+            </Link>
+          <Link className="focus:text-gray-300" to="/about">
+            <h2 className="text-xl md:text-3xl transition ease-in-out hover:scale-110 focus:active:text-gray-400">About</h2>
+            </Link>
+          <Link className="focus:text-gray-300" to="/techprojects">
+            <h2 className="text-xl md:text-3xl transition ease-in-out hover:scale-110 focus:text-gray-400">Projects</h2>
+          </Link>
+          <Link className="focus:text-gray-300" to="/skills">
+          <h2 className="text-xl md:text-3xl transition ease-in-out hover:scale-110 focus:text-gray-400">Skills</h2>
+          </Link>
+          <Link className="focus:text-gray-300" to="/contact">
+          <h2 className="text-xl md:text-3xl transition ease-in-out hover:scale-110 focus:text-gray-400">Contact</h2>
+          </Link>
+        </div>
     </div>
   )
 }
