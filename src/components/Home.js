163315f440e884541ee9a829776535f713b42c2e
Daniel Boley <danboley1@gmail.com>
2024-08-15 14:37:03 -0400

    adjust home photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,9 @@ function Home() {
       <div className="max-w-screen-xl m-auto md:grid grid-cols-3 sm:py-16 px-2 gap-8">
         <div className="pr-8">
           <img
-            alt="Daniel Boley headshot"
+            alt="Daniel Boley"
             className="w-full h-auto rounded-full m-4 shadow-xl shadow-gray-400"
-            src="https://i.imgur.com/RtNOPZb.jpg"
+            src="https://i.imgur.com/aCpVPAd.jpg"
           ></img>
         </div>
         <div className="col-span-2 my-8 mx-2 sm:pt-20">
